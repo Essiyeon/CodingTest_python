b2af9adbfcc1f4c095354a135b516dd9ea04735c
siyeon <100012844+Essiyeon@users.noreply.github.com>
2025-01-20 13:51:20 +0900

    [Bronze III] Title: 학점계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.py
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -1,31 +1,15 @@
 C = input()
-if C[0] == 'A':
-    if C[1] =='+':
-        print(4.3)
-    if C[1] =='0':
-        print(4.0)
-    if C[1] =='-':
-        print(3.7)
-elif C[0] == 'B':
-    if C[1] =='+':
-        print(3.3)
-    if C[1] =='0':
-        print(3.0)
-    if C[1] =='-':
-        print(2.7)
-elif C[0] == 'C':
-    if C[1] =='+':
-        print(2.3)
-    if C[1] =='0':
-        print(2.0)
-    if C[1] =='-':
-        print(1.7)
-elif C[0] == 'D':
-    if C[1] =='+':
-        print(1.3)
-    if C[1] =='0':
-        print(1.0)
-    if C[1] =='-':
-        print(0.7)
-else:
-    print(0.0)
+grade = {'A+': 4.3, 
+         'A0': 4.0,
+         'A-': 3.7,
+         'B+': 3.3,
+         'B0': 3.0, 
+         'B-': 2.7,
+         'C+': 2.3, 
+         'C0': 2.0,
+         'C-': 1.7,
+         'D+': 1.3,
+         'D0': 1.0, 
+         'D-': 0.7,
+         'F': 0.0 }
+print(grade[C])
