d7c114f8cf28f0c9e4119ac78919032a18713907
siyeon <100012844+Essiyeon@users.noreply.github.com>
2025-01-20 13:46:54 +0900

    [Bronze III] Title: 학점계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,31 @@
+C = input()
+if C[0] == 'A':
+    if C[1] =='+':
+        print(4.3)
+    if C[1] =='0':
+        print(4.0)
+    if C[1] =='-':
+        print(3.7)
+elif C[0] == 'B':
+    if C[1] =='+':
+        print(3.3)
+    if C[1] =='0':
+        print(3.0)
+    if C[1] =='-':
+        print(2.7)
+elif C[0] == 'C':
+    if C[1] =='+':
+        print(2.3)
+    if C[1] =='0':
+        print(2.0)
+    if C[1] =='-':
+        print(1.7)
+elif C[0] == 'D':
+    if C[1] =='+':
+        print(1.3)
+    if C[1] =='0':
+        print(1.0)
+    if C[1] =='-':
+        print(0.7)
+else:
+    print(0.0)
