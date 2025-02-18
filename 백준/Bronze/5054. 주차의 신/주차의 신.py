2d8bf5a15d20477fef9650976bd53ea102bbb88b
siyeon <100012844+Essiyeon@users.noreply.github.com>
2025-02-18 23:10:26 +0900

    [Bronze II] Title: 주차의 신, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5054. 주차의 신/주차의 신.py
+++ 백준/Bronze/5054. 주차의 신/주차의 신.py
@@ -1,12 +1,9 @@
 t = int(input())
 for _ in range(t):
   n = int(input())
-  store = input().split()
-  walk = 0
-  for i in range(len(store)):
-    store[i] = int(store[i])
+  store = list(map(int, input().split()))
   store = sorted(store)
-  # print(store)
+  walk = 0
   for i in range(1, len(store)):
     walk += store[i] - store[i-1]
   print(walk*2)
