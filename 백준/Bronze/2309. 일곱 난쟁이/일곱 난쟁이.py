699631d8b21e5e105dd67332bbdd15b3bdfa7696
siyeon <100012844+Essiyeon@users.noreply.github.com>
2025-02-21 15:34:03 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,15 @@
+height = []
+
+for _ in range(9):
+  height.append(int(input()))
+
+height.sort()
+total = sum(height)
+for i in range(len(height)):
+  for j in range(i+1,len(height)):
+    if total - height[i] - height[j] == 100:
+      for k in range(len(height)):
+        if k == i or k ==j:
+          pass
+        else: print(height[k])
+      exit()
