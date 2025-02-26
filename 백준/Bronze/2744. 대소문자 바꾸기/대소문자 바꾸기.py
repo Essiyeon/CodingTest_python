c551b397c4eab3f8eeac3f801a2f21c4f0e37f18
siyeon <100012844+Essiyeon@users.noreply.github.com>
2025-02-26 22:04:16 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,11 +1,2 @@
-string = str(input())
-string_list = list(string)
-reverse_string = ''
-for s in string_list:
-  if s.isupper() == True:
-    s = s.lower()
-    reverse_string += s
-  else:
-    s = s.upper()
-    reverse_string += s
-print(reverse_string)
+string = input()
+print(string.swapcase())
