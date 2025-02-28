686bdd80583f5e7c6feed6fdd84ff98bb89b47ba
siyeon <100012844+Essiyeon@users.noreply.github.com>
2025-02-28 17:01:08 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.py
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.py
@@ -1,9 +1,5 @@
 n = int(input())
 for _ in range(n):
   line = input()
-  line_list = list(map(str, line))
-  line_list[0] = line_list[0].upper()
-  result = ''
-  for i in range(len(line_list)):
-    result += line_list[i]
-  print(result)
+  line = line[0].upper() + line[1:]
+  print(line)
