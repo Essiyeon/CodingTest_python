7486439958c7f7ab31d1f16e72b758e10e349559
siyeon <100012844+Essiyeon@users.noreply.github.com>
2025-04-07 07:55:17 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 372 ms, Memory: 32472 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -0,0 +1,19 @@
+t = int(input())
+count = 0
+
+def recursion(s, l, r, count):
+  count += 1
+  if l >= r:
+    return 1, count
+  elif s[l] != s[r]:
+    return 0, count
+  else:
+    return recursion(s, l+1, r-1, count)
+
+def isPalindrome(s):
+  return recursion(s, 0, len(s)-1, 0)
+
+for _ in range(t):
+  s = input()
+  result, cnt = isPalindrome(s)
+  print(result, cnt)
